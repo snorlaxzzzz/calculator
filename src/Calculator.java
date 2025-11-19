356b59efae3752f6c1a7f0997abd74af3dab0154
KyriakosTerz <kyriterz3@ihu.gr>
2025-11-19 17:38:29 +0200

    ifs done

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,6 +11,42 @@ import javax.swing.JTextField;
 
 public class Calculator {
 }
+ if (e.getSource() == this.piButton) {
+        this.textfield.setText(String.valueOf(Math.PI));
+        }
+
+        if (e.getSource() == this.cosButton || e.getSource() == this.sinButton || e.getSource() == this.tanButton || e.getSource() == this.logButton || e.getSource() == this.sqrtButton) {
+        try {
+double num = Double.parseDouble(this.textfield.getText());
+double result = (double)0.0F;
+String command = e.getActionCommand();
+                if (command.equals("cos")) {
+result = Math.cos(Math.toRadians(num));
+        } else if (command.equals("sin")) {
+result = Math.sin(Math.toRadians(num));
+        } else if (command.equals("tan")) {
+result = Math.tan(Math.toRadians(num));
+        } else if (command.equals("log")) {
+        if (!(num > (double)0.0F)) {
+        this.textfield.setText("Error (Log Domain)");
+                        return;
+                                }
+
+result = Math.log10(num);
+                } else if (command.equals("sqrt")) {
+        if (!(num >= (double)0.0F)) {
+        this.textfield.setText("Error negative root");
+                        return;
+                                }
+
+result = Math.sqrt(num);
+                }
+
+                        this.textfield.setText(String.valueOf(result));
+        } catch (NumberFormatException var10) {
+        this.textfield.setText("Error");
+            }
+                    }//
 
 if (e.getSource() == this.equButton) {
         try {
